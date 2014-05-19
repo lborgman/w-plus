3c44c4201df3c391a8e9573094718fb573da7316
lborgman <lennart.borgman@gmail.com>
2014-05-19 03:45:51 +0200

    Temporay changed build => build2
    The files in the build subdir where not discoverd by GitHub for Windows.
    Changed the subdir name to fix this.
    
    Added files cw.css.js and cw.js.plovr.js

---
+++ src/build2/cw.css.js
@@ -0,0 +1,410 @@
+// Call this in the js bookmarklet file when the bookmarklet div has been added:
+function addCss_cw_css_js() {
+var temp = document.querySelectorAll("#myMenu-findword72789 * ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"box-sizing: border-box !important;"
+  +"-moz-box-sizing: border-box !important;"
+}
+
+var temp = document.querySelectorAll("#myMenu-findword72789 img, #myMenu-findword72789 label ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"display: inline !important;"
+}
+
+var temp = document.querySelectorAll("#myMenu-findword72789 label ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"float: none !important;"
+  +"text-shadow: none !important;"
+  +"color: black !important;"
+}
+
+var temp = document.querySelectorAll("#myMenu-findword72789 div ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"margin-left: 0 !important;"
+  +"margin-right: 0 !important;"
+  +"width: auto !important;"
+}
+
+var temp = document.querySelectorAll("#myMenu-findword72789 textarea ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"width: 100% !important;"
+  +"border: none !important;"
+  +"resize: both !important;"
+  +"word-wrap: normal !important;"
+  +"margin: 0 !important;"
+}
+
+var temp = document.querySelectorAll(".findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding: 0px !important;"
+  +"margin: 0px !important;"
+  +"border: none !important;"
+  +"display: inline !important;"
+  +"font: inherit !important;"
+  +"color:inherit !important;"
+  +"background-color:inherit !important;"
+  +"float:none !important;"
+  +"text-align:inherit !important;"
+  +"width:auto !important;"
+}
+
+var temp = document.querySelectorAll("span.findword72789CurrentWord ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"background: yellow !important;"
+  +"z-index: 12345 !important;"
+}
+
+var temp = document.querySelectorAll("div#tooltip-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"position: fixed;"
+  +"xposition: absolute;"
+  +"xposition: relative;"
+  +"left: 50px;"
+  +"top: 50px;"
+  +"z-index: 12345678 !important;"
+  +"background: yellow !important;"
+  +"color: black !important;"
+  +"border: 1px solid #ddd !important;"
+  +"padding: 5px !important;"
+  +"font-size: 12px !important;"
+  +"width: auto !important;"
+  +"box-shadow: 20px 25px 10px rgba(0, 0, 0, 0.5) !important;"
+}
+
+var temp = document.querySelectorAll(".containerText-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"background: white !important;"
+  +"margin: 0 !important;"
+  +"display: inline-block !important;"
+  +"position: relative !important;"
+}
+
+var temp = document.querySelectorAll(".textField-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: black !important;"
+  +"padding: 2px !important;"
+  +"padding-left: 4px !important;"
+  +"padding-right: 18px !important;"
+  +"margin: 0 !important;"
+  +"margin-right: 20px !important;"
+  +"word-wrap: normal !important;"
+}
+
+var temp = document.querySelectorAll("#on-setup-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"background-color: #f93 !important;"
+  +"padding: 40px !important;"
+  +"xborder: 2px inset red !important;"
+}
+
+var temp = document.querySelectorAll("#googleSite-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"background: linear-gradient(white, #ddd, #ccc) !important;"
+  +"color: black !important;"
+  +"margin: 0 !important;"
+  +"padding: 12px !important;"
+  +"padding-right: 0px !important;"
+  +"font-size: 12px !important;"
+}
+
+var temp = document.querySelectorAll("#googleSiteBx-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"xwidth: 12px !important;"
+  +"xheight: 12px !important;"
+  +"xmargin: 3px !important;"
+  +"xmargin-left: 4px !important;"
+  +"appearance: checkbox !important;"
+  +"-webkit-appearance: checkbox !important;"
+}
+
+var temp = document.querySelectorAll("#googleSiteURLContainer-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"min-width: 250px !important;"
+}
+
+var temp = document.querySelectorAll("#googleSiteURL-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"xpadding-right: 18px !important;"
+  +"color: #999 !important;"
+  +"width: 222px !important;"
+  +"word-wrap: break-word !important;"
+}
+
+var temp = document.querySelectorAll("#googleSiteUp-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"position: absolute !important;"
+  +"right: 2px !important;"
+  +"top: 1px !important;"
+  +"color: #ccc !important;"
+  +"cursor: pointer !important;"
+}
+
+var temp = document.querySelectorAll("#theWordsContainer-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"border:1px #eee inset !important;"
+  +"min-width: 235px !important;"
+}
+
+var temp = document.querySelectorAll("#clearWords-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"position: absolute;"
+  +"right: 2px !important;"
+  +"top: 1px !important;"
+  +"color: #ccc !important;"
+  +"cursor: pointer !important;"
+}
+
+var temp = document.querySelectorAll("div#shield-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"position: fixed !important;"
+  +"left: 0px !important;"
+  +"top: 0px !important;"
+  +"width:2000px !important;"
+  +"height:2000px !important;"
+  +"background: black !important;"
+  +"opacity: 0.3 !important;"
+  +"filter: alpha(opacity = 50) !important;"
+  +"cursor: pointer!important;"
+  +"z-index: 12345676 !important;"
+}
+
+var temp = document.querySelectorAll("#noWord-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"position: fixed !important;"
+  +"padding: 0px !important;"
+  +"max-width: 310px !important;"
+  +"z-index: 123456789 !important;"
+  +"display: none;"
+}
+
+var temp = document.querySelectorAll("#noWordInner-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding: 8px !important;"
+  +"xbackground: #fcc !important;"
+  +"background: black !important;"
+  +"color: red !important;"
+  +"opacity: 0.9 !important;"
+  +"border: 1px solid black !important;"
+  +"box-shadow: 20px 25px 10px rgba(0, 0, 0, 0.5) !important;"
+  +"position: relative !important;"
+  +"top: 12px !important;"
+  +"left: 15px !important;"
+}
+
+var temp = document.querySelectorAll("#menuFooter-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"background: #ccc !important;"
+  +"color: #990 !important;"
+  +"padding: 6px !important;"
+  +"height: auto !important;"
+  +"text-align: right !important;"
+}
+
+var temp = document.querySelectorAll("#menuFooter-findword72789 a ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: #330 !important;"
+  +"background-color: inherit !important;"
+  +"text-decoration: underline !important;"
+  +"font-size: 12px !important;"
+  +"display: inline !important;"
+  +"padding: 0px !important;"
+}
+
+var temp = document.querySelectorAll("#footMsg-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"xbackground: yellow !important;"
+  +"color: white ! important;"
+  +"margin-right: 50px !important;"
+}
+
+var temp = document.querySelectorAll("#menuHeader-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"background: #ccc !important;"
+  +"font-size: 12px !important;"
+  +"height: auto !important;"
+}
+
+var temp = document.querySelectorAll("#menuHeaderInner-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding-left: 6px !important;"
+  +"padding-right: 3px !important;"
+}
+
+var temp = document.querySelectorAll(".aLine-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding: 0 !important;"
+  +"margin: 0 !important;"
+}
+
+var temp = document.querySelectorAll("#topLine-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"xbackground: linear-gradient(175deg,  #aae9fe 0%,#85dbf5 66%,#3dcbfb 100%) !important;"
+  +"xbackground: linear-gradient(180deg,  rgba(170,233,254,1) 0%,rgba(133,219,245,0.2) 66%,rgba(61,203,251,0.0) 100%) !important;"
+  +"xbackground: linear-gradient(180deg,  rgba(204,204,204,1) 0%,rgba(225,225,225,1) 66%,rgba(250,250,255,1) 100%) !important;"
+  +"background: linear-gradient(180deg,  rgba(240,240,255,1) 0%,rgba(225,225,225,1) 33%,rgba(204,204,204,1) 100%) !important;"
+}
+
+var temp = document.querySelectorAll("#quoteLine-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding-bottom: 4px !important;"
+}
+
+var temp = document.querySelectorAll("#quoteBx-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"margin-left: 0 !important;"
+  +"margin-bottom: 0 !important;"
+}
+
+var temp = document.querySelectorAll("#quitCross-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: #aaa !important;"
+  +"float: right !important;"
+  +"cursor: pointer!important;"
+  +"width: 40px;"
+  +"text-align: right;"
+}
+
+var temp = document.querySelectorAll("#quitCross-findword72789:hover ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: yellow !important;"
+}
+
+var temp = document.querySelectorAll("#preWords-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"font-weight: bold !important;"
+  +"display: inline-block !important;"
+  +"margin-right: 2px !important;"
+}
+
+var temp = document.querySelectorAll("#postWords-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: black !important;"
+  +"margin: 0 !important;"
+  +"padding-top: 4px !important;"
+  +"padding-bottom: 0px !important;"
+  +"xdisplay: inline-block !important;"
+}
+
+var temp = document.querySelectorAll("#myMenu-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"z-index: 12345677 !important;"
+  +"xposition: absolute !important;"
+  +"border: solid 1px #CCC !important;"
+  +"width: 310px !important;"
+  +"background: #EEE !important;"
+  +"text-align: left !important;"
+  +"box-shadow: 20px 25px 10px rgba(0, 0, 0, 0.5) !important;"
+  +"line-height: normal !important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding: 0px !important;"
+  +"margin: 0px !important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"list-style: none !important;"
+  +"padding: 0px !important;"
+  +"margin: 0px !important;"
+  +"line-height: 20px !important;"
+  +"height: 20px !important;"
+  +"background: none white !important;"
+  +"font-size: 12px !important;"
+  +"text-align: left !important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p A ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: #333 !important;"
+  +"text-decoration: none !important;"
+  +"display: block !important;"
+  +"xoutline: none !important;"
+  +"padding:  0 !important;"
+  +"xpadding-left: 6px !important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p A img ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding-top:2px!important;"
+  +"margin:2px!important;"
+  +"margin-left:6px!important;"
+  +"margin-right:8px!important;"
+  +"width: 16px !important;"
+  +"height: 16px !important;"
+  +"vertical-align: top !important;"
+}
+
+var temp = document.querySelectorAll("#myMenuInner-findword72789, #myMenuInnerDyn-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"padding:0!important;"
+  +"margin:0!important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p A span ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"vertical-align:top!important;"
+  +"xpadding-left: 4px !important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p.disabled A ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"color: #AAA !important;"
+  +"cursor: default;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p.hover.disabled A ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"xbackground-color: transparent !important;"
+}
+
+var temp = document.querySelectorAll(".contextInnerMenu-findword72789 p.separator-findword72789 ");
+for (var i=0; i<temp.length; i++) {
+  temp[i].style.cssText+=""
+  +"border-top: solid 1px #CCC !important;"
+}
+}
---
+++ src/build2/cw.js.plovr.js
@@ -0,0 +1,23 @@
+// See http://www.plovr.com/options.html
+{
+    "id": "cw",
+    // "fingerprint": true,
+    "inputs": [
+        "cw.js",
+        "cw.css.js",
+        "../popupMenu.js",
+        "../caret-position.js",
+        // "../serialize.js", // Uncomment to get a log of some things like myArgs.
+    ],
+    "paths": ".",
+    "externs": [
+        "cw-externs.js",
+    ],
+    "custom-externs-only": false,
+    "mode":"advanced",
+    // "mode":"simple",
+    // "mode":"whitespace",
+    "output-file": "../../../Public/it/cw/cw-cld.js",
+    "output-wrapper": "/* Copyright 2014 Lennart Borgman, CLDDATE */ (function(){%output%})();",
+    "output-charset": "UTF-8"
+}
